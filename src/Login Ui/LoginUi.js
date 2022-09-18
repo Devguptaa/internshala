d7d95e64396127f28ec0042dea1a665164d94b77
Dev <devgupta5552@gmail.com>
2022-09-19 00:05:29 +0530

    initial commit

---
+++ src/Login Ui/LoginUi.js
@@ -0,0 +1,44 @@
+import './LoginUi.css';
+import profile from "./../image/a.png";
+import emaill from "./../image/emaill.jpg";
+import password from "./../image/passw.jpg";
+function LoginUi() {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           {/* <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div> */}
+
+
+         </div>
+         <div className="downn">
+           <div>
+             <img src={emaill} alt="email" className="email"/>
+             <input type="text" placeholder="Email" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={password} alt="pass" className="email"/>
+             <input type="password" placeholder="Password" className="name"/>
+           </div>
+            <p className="link">
+            <input type="checkbox" id="check" className="checkbox-round"/>
+            <a href="#">Remember Me</a> &nbsp;&nbsp;&nbsp; <a href="#">Forgot Password?</a>
+            </p>
+            <div className="login-button">
+          <button>Login</button>
+          </div>
+          <p className="signup">
+            <a href="i">Don't have an account? <u>Sign up</u></a>
+          </p>
+          </div>
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default LoginUi;
